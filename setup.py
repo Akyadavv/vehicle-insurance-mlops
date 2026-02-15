92dfdc0f477dbfd62e7eced43801c846da06dc0e
Ankit Kumar <134193472+Akyadavv@users.noreply.github.com>
2026-02-15 15:25:23 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankit kumar",
+    author_email="ankityadavworks@gmail.com",
+    packages=find_packages()
+)
